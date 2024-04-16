a1668e2ab70a8b17f5f11114b24d79e2ada01f64
fabianNav <fn39001@gmail.com>
2024-04-16 19:29:36 -0300

    Creamos la funcion addEvent

--- main.js
+++ main.js
@@ -13,4 +13,22 @@ document.getElementById('formEvents').addEventListener('submit', (e) => {
   addEvent();
 })
 
+function addEvent() {     //? Esta funcion hace que todas las funciones se junten y de ahí se van al evento principal.
+  if(eventName.value === "" || eventDate.value === "") {
+    return;
+  } else if (dateDiff(eventDate.value) < 0) {
+    return;
+  } 
 
+  const newEvent = {
+    id: (Math.random() * 200).toString(36).slice(3),
+    name: eventName.value,
+    date: eventDate.value
+  };
+
+  events.unshift(newEvent);
+
+  eventName.value = "";
+
+  renderEvents();
+}

3823dab51162c4b764880d2cb68d0b14dbaf119b
fabianNav <fn39001@gmail.com>
2024-04-16 19:17:42 -0300

    Creamos las variables

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import './css/style.css'
+
+let events = [];
+let arr = []; //? Sirve para cargar informacion y asignarla a eventos.
+
+const eventName = document.getElementById('eventName');
+const eventDate = document.getElementById('eventDate');
+const buttonAdd = document.getElementById('bAdd');
+const eventsContainer = document.getElementById('eventsContainer');
+
+
+
+
+
